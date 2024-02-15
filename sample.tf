f319be5e59bd04d8232b6549bf1ff2d015c4e7ac
Pratikesh05 <157086376+Pratikesh05@users.noreply.github.com>
2024-02-15 21:07:22 +0530

    qwe

--- sample.tf
+++ sample.tf
@@ -3,8 +3,8 @@ provider= "aws"{
 }
  
  resource= "aws_instance" "my_instance"{
-    ami= "var.image_id"
-    instance_type= "t2_micro"
-    key_pai= "canada-keypairr"
-    vpc_security_groups_id= "default"
+     ami= "var.image_id"
+     instance_type= "t2_micro"
+     key_pai= "canada-keypairr"
+     vpc_security_groups_id= "default"
  }
