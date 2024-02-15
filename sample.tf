0b8b681a16a9bec6c5057c87f7c178ad4a01d771
Pratikesh05 <157086376+Pratikesh05@users.noreply.github.com>
2024-02-15 11:31:15 +0530

    basic

---
+++ sample.tf
@@ -0,0 +1,10 @@
+provider= "aws"{
+    region= "ca-central-1"
+}
+ 
+ resource= "aws_instance" "my_instance"{
+    ami= "var.image_id"
+    instance_type= "t2_micro"
+    key_pai= "canada-keypairr"
+    vpc_security_groups_id= "default"
+ }
