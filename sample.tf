269a79ab69e5e5b2b139ca99d3abcfc060cca77f
Pratikesh05 <157086376+Pratikesh05@users.noreply.github.com>
2024-02-16 09:29:52 +0530

    asd

--- sample.tf
+++ sample.tf
@@ -6,7 +6,7 @@ provider "aws"{
      ami = var.image_id
      instance_type = "t2_micro"
      key_name = "canada-keypairr"
-     vpc_security_groups_ids = var.vpc_security_groups_ids
+     vpc_security_groups_ids = "sg-0d0cb75235df91852"
      tags = {
          name = "pavan"
      }
@@ -18,7 +18,4 @@ provider "aws"{
     }
 
 
-  variable  "vpc_security_groups_ids" {
-     default = "sg-0d0cb75235df91852"
-    }
     
