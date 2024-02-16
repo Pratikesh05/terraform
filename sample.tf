0f415f1fc69fb1cd1875b7172a89d5563272f68f
Pratikesh05 <157086376+Pratikesh05@users.noreply.github.com>
2024-02-16 09:23:25 +0530

    asd

--- sample.tf
+++ sample.tf
@@ -2,13 +2,13 @@ provider "aws"{
     region = "ca-central-1"
 }
  
- resource"aws_instance" "my_instance"{
+ resource"aws_instance" "my_instance" {
      ami = var.image_id
      instance_type = "t2_micro"
      key_pair = "canada-keypairr"
      vpc_security_groups_id = var.vpc_security_groups_id
-     tags={
-        name = "pavan"
+     tags = {
+         name = "pavan"
      }
  }
 
