c8c5587febe6e18cfddb61ebb9e6d5ce7f2b8994
Pratikesh05 <157086376+Pratikesh05@users.noreply.github.com>
2024-02-16 09:00:13 +0530

    qwer

--- sample.tf
+++ sample.tf
@@ -6,5 +6,17 @@ provider= "aws"{
      ami= "var.image_id"
      instance_type= "t2_micro"
      key_pair= "canada-keypairr"
-     vpc_security_groups_id= "default"
+     vpc_security_groups_id= var.vpc_security_groups_id
+     tags={
+        name= "pavan"
+     }
  }
+
+
+ variable= "image_id"{
+    default= "ami-0a2e7efb4257c0907"}
+
+
+  variable= "vpc_security_groups_id"{
+    default= "sg-0d0cb75235df91852"}
+    
