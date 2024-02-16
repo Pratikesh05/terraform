e38f0e1337c607e4c208c1da8205a784cd1908fa
Pratikesh05 <157086376+Pratikesh05@users.noreply.github.com>
2024-02-16 09:18:03 +0530

    asdf

--- sample.tf
+++ sample.tf
@@ -1,22 +1,22 @@
 provider "aws"{
-    region= "ca-central-1"
+    region = "ca-central-1"
 }
  
- resource= "aws_instance" "my_instance"{
-     ami= var.image_id
-     instance_type= "t2_micro"
-     key_pair= "canada-keypairr"
-     vpc_security_groups_id= var.vpc_security_groups_id
+ resource"aws_instance" "my_instance"{
+     ami = var.image_id
+     instance_type = "t2_micro"
+     key_pair = "canada-keypairr"
+     vpc_security_groups_id = var.vpc_security_groups_id
      tags={
-        name= "pavan"
+        name = "pavan"
      }
  }
 
 
- variable= "image_id"{
-    default= "ami-0a2e7efb4257c0907"}
+ variable "image_id"{
+    default = "ami-0a2e7efb4257c0907"}
 
 
-  variable= "vpc_security_groups_id"{
-    default= "sg-0d0cb75235df91852"}
+  variable  "vpc_security_groups_id"{
+    default = "sg-0d0cb75235df91852"}
     
