dd558bc0252f3c2ac59673f6758c101ec082aa0b
Pratikesh05 <157086376+Pratikesh05@users.noreply.github.com>
2024-02-19 09:31:27 +0530

    asdfg

--- advance-sample.tf
+++ advance-sample.tf
@@ -10,9 +10,21 @@ resource "aws_instance" "my_instance{
      tags = { 
          name = instance_01 }
 
+resource "aws_instance" "my_anotherinstance{
+     ami = var.image_id
+     instance_type = "t3_micro"
+     key_name = "canda keyparir"
+     vpc_security_group_ids = []
+     tags = { 
+         name = instance_0r }
 
 } 
  variable "image_id" {
     default = 
     description = "(optional) describe your variable"
  }
+
+ variable "image_id"{
+    default = 
+ } 
+    
